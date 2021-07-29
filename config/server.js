60e80fb812395eb1723cabe0fca13325ef9c62d1
dm1tro69 <dimolg22@gmail.com>
2021-07-29 17:04:49 +0300

    add events

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'd253192c76ca3dc212b807d5bf4df82e'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '04a8b2e1-463c-4de0-95a4-a559ad12db1b'
+};
